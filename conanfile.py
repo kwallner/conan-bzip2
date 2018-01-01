d90570a155e90abb1b13f1fcfd932a8efb38c7b6
Karl Wallner <karl.wallner@gmx.de>
2018-01-02 00:59:01 +0100

    Support shared build. Reworked CMake-Integration.

--- conanfile.py
+++ conanfile.py
@@ -11,16 +11,18 @@ class Bzip2Conan(ConanFile):
     branch = "master"
     generators = "cmake"
     settings = "os", "compiler", "arch", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = "shared=False", "fPIC=True"
-    exports = ["CMakeLists.txt"]
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
     url = "https://github.com/kwallner/conan-bzip2"
     license = "BSD-style license"
     description = "bzip2 is a freely available, patent free (see below), high-quality data " \
                   "compressor. It typically compresses files to within 10% to 15% of the best" \
                   " available techniques (the PPM family of statistical compressors), whilst " \
                   "being around twice as fast at compression and six times faster at decompression."
-
+    exports = ["CMakeLists.txt"]
+    exports_sources = ["patches/bzip2-1.0.6_modern_cmake_build.patch"]
+    no_copy_source = True
+    
     @property
     def zip_folder_name(self):
         return "bzip2-%s" % self.version
@@ -34,28 +36,17 @@ class Bzip2Conan(ConanFile):
         tools.check_md5(zip_name, "00b516f4704d4a7cb50a1d97e6e8e15b")
         tools.unzip(zip_name)
         os.unlink(zip_name)
-
-    def build(self):
+        
+        # Prepare for CMake
         shutil.move("CMakeLists.txt", "%s/CMakeLists.txt" % self.zip_folder_name)
-        with tools.chdir(self.zip_folder_name):
-            os.mkdir("_build")
-            with tools.chdir("_build"):
-                cmake = CMake(self)
-                if self.options.fPIC:
-                    cmake.definitions["FPIC"] = "ON"
-                cmake.configure(build_dir=".", source_dir="..")
-                cmake.build(build_dir=".")
-
-    def package(self):
-        self.copy("*.h", "include", "%s" % self.zip_folder_name, keep_path=False)
-        self.copy("*bzip2", dst="bin", src=self.zip_folder_name, keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", src=self.zip_folder_name, keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", src=self.zip_folder_name, keep_path=False)
-        self.copy(pattern="*.a", dst="lib", src="%s/_build" % self.zip_folder_name, keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", src="%s/_build" % self.zip_folder_name, keep_path=False)
-        self.copy(pattern="*.dll", dst="bin", src="%s/_build" % self.zip_folder_name, keep_path=False)
+        tools.patch(patch_file="patches/bzip2-1.0.6_modern_cmake_build.patch")
+    
+    def build(self):
+        cmake = CMake(self)
+        cmake.definitions["BUILD_SHARED_LIBS"]= self.options.shared
+        cmake.configure(source_dir="%s/%s" % (self.source_folder, self.zip_folder_name))
+        cmake.build()
+        cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ['bz2']
-        if self.settings.build_type == "Debug":
-            self.cpp_info.libs[0] += "d"
+        self.cpp_info.libs = [ "bz2%s" % ("d" if self.settings.build_type == "Debug" else "") ]
